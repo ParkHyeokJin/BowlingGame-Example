ff584bc6b5368811b3647a447b350fc8025b39fc
ParkHyeokJin <parkhjpp@daum.net>
2019-06-27 16:37:51 +0900

    Refactoring - Remove dup

--- src/test/java/BowlingTest.java
+++ src/test/java/BowlingTest.java
@@ -5,13 +5,7 @@ public class BowlingTest {
     private Game game;
 
     @Test
-    public void canCreate(){
-        game = new Game();
-    }
-
-    @Test
     public void canRoll() {
-        Game game = new Game();
         game.roll(0);
     }
 }
