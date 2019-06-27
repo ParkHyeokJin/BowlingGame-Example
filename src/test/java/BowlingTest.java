1864c22378239757a98221d0f4ff98ca4b867762
ParkHyeokJin <parkhjpp@daum.net>
2019-06-27 16:41:58 +0900

    add Failing test - add method allOnes()

--- src/test/java/BowlingTest.java
+++ src/test/java/BowlingTest.java
@@ -18,4 +18,13 @@ public class BowlingTest {
             game.roll(0);
         assertThat(game.score(), is(0));
     }
+
+    @Test
+    public void allOnes() {
+        for(int i = 0; i < 20; i++)
+            game.roll(1);
+        assertThat(game.score(), is(20));
+    }
+
+
 }
